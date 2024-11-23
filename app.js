498d6cb7cdf15f742bac0e907718fbe9d94bceaf
aagii <altangerelmaakhuu60@gmail.com>
2024-11-23 19:24:27 +0800

    Shoo shideh uildliig programchlah

--- app.js
+++ app.js
@@ -8,9 +8,6 @@ var roundScores = 0;
 //Шоо аль талаараа буусныг хадгалах хувьсагч 1-6 гэсэн тоог авна//
 //var diceNumber = Math.floor(Math.random() * 6) + 1;
 
-//window.document.querySelector("#score-0").textContent = dice;
-//document.querySelector("#score-0").innerHTML = <em>Yes!</em>;
-//window.document.querySelector("#score-0").textContent = 0;
 // Программ эхлэхэд бэлтгэх
 document.getElementById("score-0").textContent = 0;
 document.getElementById("score-1").textContent = 0;
@@ -20,13 +17,33 @@ document.getElementById("current-1").textContent = 0;
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
 //addEventListener("click", shooshid) 2аргумент авна Эхнийх ямар үйлдэл хийх(click), 2 дахь нь click eventeer юу хийх(shooshid)
+// шоог шидэх ивэнт листнер
 document.querySelector(".btn-roll").addEventListener("click", function() {
+    //1-6 хүртэлх санамсаргүй тоог гаргаж авна
     var diceNumber = Math.floor(Math.random() * 6) + 1;
-    //alert("шоог шидлээ:" + diceNumber);
-    document.querySelector(".dice").style.display = "block";
-    document.querySelector(".dice").src = "dice-" + diceNumber + ".png";
+    // Шооны зургийг вэб дээр гаргана
+    diceDom.style.display = "block";
+    // буусан санамсаргүй тоонд харгалзах шооны зургийг вэб дээр гаргаж ирнэ
+    diceDom.src = "dice-" + diceNumber + ".png";
+    // Буусан тоо нь 1-ээс ялгаатай бол тоглогчийн ээлжийн оноог нэмэгдүүлнэ
+    if(diceNumber !== 1){
+        // 1-ээс ялгаатай тоо буулаа. Буусан тоог тоглогчид нэмж өгнө
+        roundScores = roundScores + diceNumber;
+        document.getElementById("current-" + activePlayer).textContent = roundScores;
+    }else{
+        // тоглогчийн ээлжиндээ цуглуулсан оноог 0 болгоно
+        roundScores = 0;
+        document.getElementById("current-" + activePlayer).textContent = 0;
+        // 1 буусан тул тоглогчийн оноог 0 болгож ээлжийг шилжүүлнэ
+        activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+        // улаан цэгийг шилжүүлэхremove add
+        document.querySelector(".player-0-panel").classList.toggle("active");
+        document.querySelector(".player-1-panel").classList.toggle("active");
+        //Шооны зургийг алга болгох
+        diceDom.style.display = "none";
+    }
 });
-
-// function shooshid() {
-//     var diceNumber = Math.floor(Math.random() * 6) + 1;
-// }
+// Hold товч дарж цуглуулсан оноогоо хадгалж авах
+Document.querySelector(".btn-hold").addEventListener("click", function(){
+    scores[0] = scores[0] + 
+});
